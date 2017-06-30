76555e168c2a2afbfb5f1d06b36bff73a797760f
Adrien Rogliano <rogliano.adrien@hotmail.fr>
2017-06-30 14:23:44 +0200

    add test LEDs

--- test_matrice_leds/test_matrice_leds.ino
+++ test_matrice_leds/test_matrice_leds.ino
@@ -1,10 +1,15 @@
+byte ligne[3] = {2, 3, 4};
+byte colonne[3] = {5, 6, 7};
+
 void setup(){
     for (int i = 2; i < 8; i++){
         pinMode(i, OUTPUT);
+        if (i >= 5){
+            digitalWrite(i, HIGH);
+        }
     }
 }
 void loop(){
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-
+    digitalWrite(ligne[1], HIGH);
+    digitalWrite(colonne[1], LOW);
 }
