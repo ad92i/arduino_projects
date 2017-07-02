42261ef12d22ba482755a33434bc85a2b295ef2c
Adrien Rogliano <rogliano.adrien@hotmail.fr>
2017-07-02 18:11:13 +0200

    fonctionnel !

--- Joystick_LEDs_5x5/Joystick_LEDs_5x5.ino
+++ Joystick_LEDs_5x5/Joystick_LEDs_5x5.ino
@@ -1,14 +1,43 @@
+#define TAILLE_MATRICE 5
+
+const int pinLigne[TAILLE_MATRICE] = {2, 3, 4, 5, 6};
+const int pinColonne[TAILLE_MATRICE] = {8, 9, 10, 11, 12};
+
 void setup(){
-    for (int i = 2; i < 13; i++){
-        pinMode(i, OUTPUT);
-/*        if (i > 6){
-            digitalWrite(i, HIGH);
-        }*/
-        if (i < 7){
-            digitalWrite(i,HIGH);
-        }
+    for (int i = 0; i < TAILLE_MATRICE; i++){
+        pinMode(pinLigne[i], OUTPUT);
+        pinMode(pinColonne[i], OUTPUT);
     } 
 }
 void loop(){
-
+    displayMatriceLEDs(map(analogRead(5),0, 1023, 0, 5), map(analogRead(0),0, 1023,  0, 5));
+    delay(10);
+}
+void allHIGH(char ligne_ou_colonne){
+    if (ligne_ou_colonne == 'c'){
+        for (int i = 0; i < TAILLE_MATRICE; i++){
+            digitalWrite(pinColonne[i], HIGH);
+        }
+    } else if (ligne_ou_colonne == 'l'){
+        for (int i = 0; i < TAILLE_MATRICE; i++){
+            digitalWrite(pinLigne[i], HIGH);
+        }
+    } 
+}
+void allLOW(char ligne_ou_colonne){
+    if (ligne_ou_colonne == 'c'){
+        for (int i = 0; i < TAILLE_MATRICE; i++){
+            digitalWrite(pinColonne[i], LOW);
+        }
+    } else if (ligne_ou_colonne == 'l'){
+        for (int i = 0; i < TAILLE_MATRICE; i++){
+            digitalWrite(pinLigne[i], LOW);
+        }
+    } 
+}
+void displayMatriceLEDs(int x, int y){
+    allLOW('l');
+    allHIGH('c');
+    digitalWrite(pinLigne[x], HIGH);
+    digitalWrite(pinColonne[y], LOW);
 }
